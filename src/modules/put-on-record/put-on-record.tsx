2ecd5eda5523a74e3a04ba4930d0b92abb673e6b
wangguoyan <2282940593@qq.com>
2020-03-31 02:04:13 +0800

    - 网站搭建中首页

--- src/modules/put-on-record/put-on-record.tsx
+++ src/modules/put-on-record/put-on-record.tsx
@@ -1,16 +1,45 @@
 import React from "react";
+import './put-on-record.css';
 
 export class PutOutRecordInfo extends React.Component {
     render() {
         return (
-            <a
-                className="App-link"
-                href="http://beian.miit.gov.cn"
-                target="_blank"
-                rel="noopener noreferrer"
+            <span
+                className="info-box"
             >
+                <a
+                    className="put-on-record"
+                    href="http://beian.miit.gov.cn"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    辽ICP备20002112号-1
+                </a>
+            </span>
+        )
+    }
+}
 
-            </a>
+export class CopyRightInfo extends React.Component {
+    render() {
+        return (
+            <span
+                className="info-box"
+            >
+                Copyright (c) 2019 migao.com 版权所有
+            </span>
+        )
+    }
+}
+
+export class StatementInfo extends React.Component {
+    render() {
+        return (
+            <span
+                className="info-box"
+            >
+                本站资源来源于网络以及个人分享，仅供个人学习与交流 
+            </span>
         )
     }
 }
